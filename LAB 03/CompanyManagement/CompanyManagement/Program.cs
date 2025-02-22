b4cb54da36e0811a31262943338be0198110837e
thanhkien2005 <thanhkienu08@gmail.com>
2025-02-22 21:41:09 +0700

    Add files via upload

---
+++ LAB 03/CompanyManagement/CompanyManagement/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainData;
+
+namespace CompanyManagement
+{
+    class Program
+    {
+        static void UpdateNumberOfCustomers(Company company)
+        {
+            company.ValNumberOfCustomers = company.ListOfCustomers.Count;
+        }
+        static void Main(string [] args)
+        {
+            Company company = new Company();
+            company.ValCompanyName = "Vinhomes";
+
+            company.CompanyAddorRemoveEvent += new Company.CompanyHandler(UpdateNumberOfCustomers);
+
+            company.ListOfCustomers.Add(new Customer("C002","Nguyen Dang Khoi", "Vinhomes Grand Park S7.02", 0987654321, CustomerType.TiemNang));
+            company.ListOfCustomers.Add(new Customer("C003", "Tran Hong Tinh", "Vinhomes Grand Park S5.03", 0135792468, CustomerType.CanQuanTam));
+            company.ListOfCustomers.Add(new Customer("C004", "KevinSanjaya", "Vinhomes Grand Park S7.02", 0246813579, CustomerType.KhachHangKhac));
+
+            Customer customer = company.SearchCustomer("C002");
+            company.RemoveCustomer(customer);
+
+            company.CompanyInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+namespace MainData
+{
+    public enum CustomerType { TrungThanh, TiemNang, CanQuanTam, KhachHangKhac };
+
+    public class Customer
+    {
+        string CustomerID = "C001";
+        string CustomerName = "Nguyen Thanh Kien";
+        string CustomerAddress = "Vinhomes Grand Park S1.02";
+        int CustomerPhone = 0123456789;
+        CustomerType type = CustomerType.TrungThanh;
+        
+        public Customer() {}
+        public Customer(string CustomerID, string CustomerName, string CustomerAddress, int CustomerPhone, CustomerType type)
+        {
+            this.CustomerID = CustomerID;
+            this.CustomerName = CustomerName;
+            this.CustomerAddress = CustomerAddress;
+            this.CustomerPhone = CustomerPhone;
+            this.type = type;
+        }
+        public string ValCustomerID
+        {
+            get { return CustomerID; }
+            set { CustomerID = value; }
+        }
+        public string ValCustomerName
+        {
+            get { return CustomerName; }
+            set { CustomerName = value; }
+        }
+        public string ValCustomerAddress
+        {
+            get { return CustomerAddress; }
+            set { CustomerAddress = value; }
+        }
+        public int ValCustomerPhone
+        {
+            get { return CustomerPhone; }
+            set { CustomerPhone = value; }
+        }
+        public CustomerType valtype
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public void CustomerInfo()
+        {
+            string typeName = Enum.GetName(typeof(CustomerType), type);
+
+            if(CustomerID  != "C001")
+                Console.WriteLine("Khach Hang {0} co ten la {1}, dia chi la {2}, so dien thoai la {3}, \nLa dang khach hang {4}\n",
+                    CustomerID, CustomerName, CustomerAddress, CustomerPhone, typeName);
+        }
+    }
+    public class Company
+    {
+        public List<Customer> ListOfCustomers { get; set; }
+        Dictionary<CustomerType, string> TypeInfo = new Dictionary<CustomerType, string>();
+
+        public delegate void CompanyHandler (Company company);
+        public event CompanyHandler CompanyAddorRemoveEvent;
+
+        string CompanyName;
+        int NumberOfCustomers = 0;
+
+        public int ValNumberOfCustomers
+        {
+            get { return NumberOfCustomers; }
+            set { NumberOfCustomers = value; }
+
+        }
+        public Company()
+        {
+            ListOfCustomers = new List<Customer>();
+            CompanyName = "Not Assigned";
+            TypeInfo.Add(CustomerType.TiemNang, "Khach hang nay co tiem nang gia han hop dong thue va mua ban nha");
+            TypeInfo.Add(CustomerType.CanQuanTam, "Khach hang nay can co nhieu thoi gian de tim hieu de co the thue hoac mua nha");
+            TypeInfo.Add(CustomerType.KhachHangKhac, "Khach hang nay khong co du dinh mua hay thue nha");
+            TypeInfo.Add(CustomerType.TrungThanh, "Khach hang nay chac chan se thue hoac mua nha");
+        }
+
+        public string ValCompanyName
+        {
+            get { return CompanyName; }
+            set { CompanyName = value; }
+        }
+
+        public void CompanyInfo()
+        {
+            Console.WriteLine("Cong ty {0} co {1} khach hang. Gom cac khach hang nhu sau:\n", CompanyName, NumberOfCustomers);
+            foreach (Customer rm in ListOfCustomers)
+            {
+                KeyValuePair<CustomerType, string> info = TypeInfo.FirstOrDefault(o => o.Key == rm.valtype);
+                rm.CustomerInfo();
+                Console.WriteLine("----Thong Tin Khach Hang: {0}\n", info.Value);
+            }
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            ListOfCustomers.Add(customer);
+            OnCompanyChanger(this);
+        }
+
+        public void RemoveCustomer(Customer customer)
+        {
+            ListOfCustomers.Remove(customer);
+            OnCompanyChanger(this);
+        }
+
+        public void OnCompanyChanger(Company company)
+        {
+            if (CompanyAddorRemoveEvent != null)
+            {
+                CompanyAddorRemoveEvent(this);
+            }
+        }
+        public Customer SearchCustomer<T> (T search)
+        {
+            Customer r = new Customer();
+            if (typeof(T) == typeof(string))
+            {
+                r = ListOfCustomers.FirstOrDefault(o => o.ValCustomerID == search.ToString());
+                if (r != null)
+                    return r;
+            }
+            else if (typeof(T) == typeof(int))
+            {
+                if (Convert.ToInt32(search) < ListOfCustomers.Count)
+                    return ListOfCustomers[Convert.ToInt32(search)];
+            }
+            Console.WriteLine("Customer is not found");
+            return new Customer();
+        }
+    }
+    public static class MyExtensions
+    {
+        public static string ConvertToString(this Customer customer)
+        {
+            string typeName = Enum.GetName(typeof(CustomerType), customer.valtype);
+
+            if (customer.ValCustomerID != "C001")
+                return String.Format("Khach Hang: {0} - {1} - {2} - {3} la loai khach hang {4}",
+                    customer.ValCustomerID, customer.ValCustomerName, customer.ValCustomerAddress, customer.ValCustomerPhone, typeName);
+            else
+                return " ";
+        }
+    }
+}
---
+++ LAB 03/HotelManagement/HotelManagement/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainData;
+
+namespace HotelManagement
+{
+    class Program
+    {
+        static void UpdateNumberOfRooms(Hotel hotel)
+        {
+            hotel.ValNumberOfRoom = hotel.Rooms.Count;
+        }
+        static void Main(string[] args)
+        {
+            Hotel hotel = new Hotel();
+            hotel.ValName = "Ngoi Sao";
+
+            hotel.HotelAddorRemoveEnvent += new Hotel.HotelHandler(UpdateNumberOfRooms);
+
+            hotel.Rooms.Add(new Room("R001", ConditionType.Standard, BedType.DoubleBedRoom, 2));
+            hotel.Rooms.Add(new Room("R002", ConditionType.Superior, BedType.TripleBedRoom, 3));
+            hotel.Rooms.Add(new Room("R003", ConditionType.Deluxe, BedType.ExtraBedRoom, 4));
+
+            Room room = hotel.SearchRoom("R001");
+            hotel.RemoveRoom(room);
+
+            hotel.HotelInfo();
+
+            Console.ReadKey();
+
+            //Console.WriteLine("Searching Process:");
+            //hotel.SearchRoom("R001").RoomInfo();
+            //hotel.SearchRoom("R004").RoomInfo();
+            //hotel.SearchRoom(2).RoomInfo();
+            //hotel.SearchRoom(3).RoomInfo(); 
+
+            //Console.WriteLine("Searching Process:");
+            //Console.WriteLine(hotel.SearchRoom("R001").ConvertToString());
+            //Console.WriteLine(hotel.SearchRoom("R004").ConvertToString());
+            //Console.WriteLine(hotel.SearchRoom(2).ConvertToString());
+            //Console.WriteLine(hotel.SearchRoom(3).ConvertToString());
+
+            //Console.ReadKey();
+        }
+    }
+}
+
+
+namespace MainData
+{
+    public enum ConditionType { Standard, Superior, Deluxe, Suite };
+    public enum BedType { ConnectingRoom, SingleBedRoom, TwinBedRoom, DoubleBedRoom, TripleBedRoom, ExtraBedRoom };
+    public class Room
+    {
+        string RoomID = "RM000";
+        ConditionType condition = ConditionType.Standard;
+        BedType bedtype = BedType.DoubleBedRoom;
+        int Capacity = 2;
+
+        public Room() { }
+        public Room(string roomID, ConditionType condition, BedType bedType, int Capacity)
+        {
+            this.RoomID = roomID;
+            this.condition = condition;
+            this.bedtype = bedType;
+            this.Capacity = Capacity;
+        }
+
+        public string ValRoomID
+        {
+            get { return RoomID; }
+            set { RoomID = value; }
+        }
+
+        public ConditionType Valcondition
+        {
+            get { return condition; }
+            set { condition = value; }
+        }
+        public BedType Valbedtype
+        {
+            get { return bedtype; }
+            set { bedtype = value; }
+        }
+        public int ValCapacity
+        {
+            get { return Capacity; }
+            set { Capacity = value; }
+        }
+
+        public void RoomInfo()
+        {
+            string conditionName = Enum.GetName(typeof(ConditionType), condition);
+            string bedtypeName = Enum.GetName(typeof(BedType), bedtype);
+
+            if (RoomID != "RM000")
+                Console.WriteLine("Phong: {0} - {1} - {2} for {3} persons", RoomID, conditionName, bedtypeName, Capacity);
+        }
+    }
+    public class Hotel
+    {
+        public List<Room> Rooms { get; set; }
+        Dictionary<ConditionType, string> conditionInfo = new Dictionary<ConditionType, string>();
+
+        public delegate void HotelHandler(Hotel hotel);
+        public event HotelHandler HotelAddorRemoveEnvent;
+
+        string Name;
+        int NumberOfRoom = 0;
+
+        public int ValNumberOfRoom
+        {
+            get {  return NumberOfRoom;}
+            set { NumberOfRoom = value;}
+        }
+        public Hotel()
+        {
+            Rooms = new List<Room>();
+            Name = "Not Assigned";
+            conditionInfo.Add(ConditionType.Standard, "Phong Standard la phong co chat luong va gia thap nhat.");
+            conditionInfo.Add(ConditionType.Superior, "Phong Superior co dien tich lon hoac huong nhin dep.");
+            conditionInfo.Add(ConditionType.Deluxe, "Phong Deluxe co chat luong cao, thuong o tang cao.");
+            conditionInfo.Add(ConditionType.Suite, "Phong Suite o tang cao nhat voi cac trang bi va dich vu dac biet.");
+        }
+        public string ValName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public void HotelInfo()
+        {
+            Console.WriteLine("Khach San {0} co {1} phong. Gom cac phong nhu sau:\n", Name, NumberOfRoom);
+            foreach (Room rm in Rooms)
+            {
+                KeyValuePair<ConditionType, string> info = conditionInfo.FirstOrDefault(o => o.Key == rm.Valcondition);
+                rm.RoomInfo();
+                Console.WriteLine("---Thong Tin Phong: {0}\n", info.Value);
+            }
+        }
+
+        public void AddRoom(Room room)
+
+        {
+            Rooms.Add(room);
+            OnHotelChanger(this);
+        }
+
+        public void RemoveRoom(Room room)
+        {
+            Rooms.Remove(room);
+            OnHotelChanger(this);
+        }
+
+        public void OnHotelChanger(Hotel hotel)
+        {
+            if (HotelAddorRemoveEnvent !=  null)
+            {
+                HotelAddorRemoveEnvent(this);
+            }
+        }
+        public Room SearchRoom<T>(T search)
+        {
+            Room r = new Room();
+            if (typeof(T) == typeof(string))
+            {
+                r = Rooms.FirstOrDefault(o => o.ValRoomID == search.ToString());
+                if (r != null)
+                    return r;
+            }
+            else if (typeof(T) == typeof(int))
+            {
+                if (Convert.ToInt32(search) < Rooms.Count)
+                    return Rooms[Convert.ToInt32(search)];
+            }
+            Console.WriteLine("Room is not found");
+            return new Room(); // Room 004 ko tồn tại, tại sao ko kiếm thấy room(3) vì chỉ số index bắt đầu từ 0,1,2.
+                               // vậy room 3 sẽ vượt quá chỉ số phòng nên ko tồn taijl.
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static string ConvertToString(this Room room)
+        {
+            string conditionName = Enum.GetName(typeof(ConditionType), room.Valcondition);
+            string bedtypeName = Enum.GetName(typeof(BedType), room.Valbedtype);
+
+            if (room.ValRoomID != "RM000")
+                return String.Format("Phong: {0} - {1} - {2} for {3} persons",
+                    room.ValRoomID, conditionName, bedtypeName, room.ValCapacity);
+            else
+                return " ";
+        }
+    }
+}
